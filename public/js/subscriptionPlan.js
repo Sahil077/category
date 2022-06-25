4ef34012b664dda26196cc8d9bcb82fd74cbf210
Sahil077 <61814218+Sahil077@users.noreply.github.com>
2022-06-25 15:44:20 +0530

    Update subscriptionPlan.js

--- public/js/subscriptionPlan.js
+++ public/js/subscriptionPlan.js
@@ -39,11 +39,57 @@ function createSubscription(data){
         }),
         success: function (output) {
             console.log('CREATE SUBSCRIPTION '+ JSON.stringify(output));
-            // verifyPayment(output)
-            document.getElementById('payforsubscription').style.display = 'block'
-            document.getElementById('pay').style.display = 'none'
-            var a = document.getElementById('payforsubscription'); //or grab it by tagname etc
-            a.href = output.short_url
+             verifyPayment(output)
+        //    document.getElementById('payforsubscription').style.display = 'block'
+        //    document.getElementById('pay').style.display = 'none'
+         //   var a = document.getElementById('payforsubscription'); //or grab it by tagname etc
+        //    a.href = output.short_url
+        },
+        error: function (err) {
+            console.log(err)
+        }
+    });
+}
+
+function verifyPayment(orderDetails) {
+    console.log(JSON.stringify(orderDetails))
+    var options = { 
+        "key": "rzp_test_umWrzSCH1vLjLL",
+        "subscription_id": orderDetails.id, 
+        "name": "InterviewHelp", 
+        "description": "Monthly Test Plan", 
+        "handler": function(response) { 
+            console.log(response)
+            razorPayverify(response)
+        } 
+     }; 
+     var paymentObject = new Razorpay(options);
+     paymentObject.open()
+}
+
+function razorPayverify(data){
+    console.log('KEYS======= ' + JSON.stringify(data))
+
+    const payment_id = data.razorpay_payment_id
+    const signature = data.razorpay_signature
+    let url = 'https://interviewhelp.me/verifypayment'
+    $.ajax({
+        type: 'POST',
+        url: url,
+        contentType: "application/json",
+        data: JSON.stringify({
+           
+            payment_id: payment_id,
+            signature: signature,
+        }),
+        success: function (output) {
+            console.log(output);
+            if(output.success == true){
+                window.open(`https://interviewhelp.me/categories`, "_self")
+            }else{
+                alert('Payment Failed, Try again Later.')
+            }
+            
         },
         error: function (err) {
             console.log(err)
