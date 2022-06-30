a5fc4d5a092751225c9adf537022d7084bc7839e
Sahil077 <61814218+Sahil077@users.noreply.github.com>
2022-06-30 14:59:08 +0530

    Update regular.js

--- public/js/regular.js
+++ public/js/regular.js
@@ -1,20 +1,28 @@
 const techValue = () => {
   const category_value = document.getElementById('techValue').value
   // console.log(category_value)
-  $('.question_section').empty();
-  $('.tags').empty();
   let url = `https://interviewhelp.me/categoryName/${category_value}`
+  // let url = `http://localhost:3000/categoryName/${category_value}`
   $.ajax({
     type: 'GET',
     url: url,
     success: function (output) {
       console.log(output);
       if (output.length > 0) {
+        $('.question_section').empty();
+        $('.tags').empty();
+        $('.tags_filter').empty();
         var str = ""
         var tags_str = ""
+        var tech_tags = ""
+        const technical_tags = []
         for (var i = 0; i < output.length; i++) {
           var categoryId = (output[i]._id).toString()
-          // var edit_div = 'question_div' + [i]
+          const showTechtags = {
+            tagID : output[i].category_name + "_" + output[i].technical_tagName,
+            techTag: output[i].technical_tagName
+          }
+          technical_tags.push(showTechtags)
           str += `<div class="row mb-4 edit_div">
                       <div class="form-group col-10 question_block" id="${output[i]._id}">
                           ${output[i].question} 
@@ -23,12 +31,15 @@ const techValue = () => {
           for (var j = 0; j < (output[i].tags).length; j++) {
             tags_str += `<li id= "${[j]}"><a class="tag" id= "${output[i].tags[j]+'_'+[j]}">${output[i].tags[j]}</a></li>`
           }
-
+        }
+        console.log(technical_tags)
+        for (var j = 0; j < technical_tags.length; j++) {
+          tech_tags += `<li ><a class="single_tag" id="${technical_tags[j].tagID}" >${technical_tags[j].techTag}</a></li>`
         }
         $('.question_section').append(str)
         $('.tags').append(tags_str)
+        $('.tags_filter').append(tech_tags)
         $('.select_Category').hide()
-        // console.log(str)
       } else {
         $('.select_Category').show()
         $('.question_section .row').hide()
@@ -45,11 +56,12 @@ $('.tags').on('click', function (event) {
   const tag_val = event.target;
   var css_id = (tag_val.id)
   var split_id = (tag_val.id).split('_')[0]
-  // before
-  $('.tag').css('background-color', '#e3edf9');
-  // after
-  $('#' + css_id).css('background-color', '#8aa9c6');
-  let url = `https://interviewhelp.me/tags/${split_id}`
+    // before
+  $('.single_tag').css('background-color', '#ffffff');
+    // after
+  $('#' + subTag_id).css('background-color', '#8aa9c6');
+  let url = `https://interviewhelp.me/tags/${split_id}` 
+  // let url = `http://localhost:3000/tags/${split_id}` 
   $.ajax({
     type: 'GET',
     url: url,
@@ -86,6 +98,7 @@ $('.question_section').on('click', function (event) {
   console.log(tag_val.id)
   $('#modal-body').empty();
   let url = `https://interviewhelp.me/categoryID/${tag_val.id}`
+  // let url = `http://localhost:3000/categoryID/${tag_val.id}`
   $.ajax({
     type: 'GET',
     url: url,
@@ -108,3 +121,58 @@ $('.question_section').on('click', function (event) {
 const close_answer = () => {
   $('#answerCard').modal('hide');
 }
+
+$('.tags_filter').on('click', function (event) {
+  const tag_val = event.target;
+  var subTag_id = tag_val.id
+  console.log(subTag_id)
+      // before
+  $('.tags_filter').css('background-color', '#ffffff');
+    // after
+  $('#' + subTag_id).css('background-color', '#8aa9c6');
+
+  const category_name = subTag_id.split("_")[0]
+  const technical_tagName = subTag_id.split("_")[1]
+  let url = `https://interviewhelp.me/user/technicalTag`
+  // let url = `http://localhost:3000/user/technicalTag`
+  $.ajax({
+    type: 'POST',
+    url: url,
+    contentType: "application/json",
+    data: JSON.stringify({
+      category_name: category_name,
+      technical_tagName:technical_tagName,
+    }),
+    success: function (output) {
+      console.log(output)
+      $('.question_section').empty();
+      $('.tags').empty();
+      if (output) {
+        var str = ""
+        var tags_str = ""
+    
+          str += `<div class="row mb-4 edit_div">
+                      <div class="form-group col-10 question_block" id="${output._id}">
+                          ${output.question} 
+                      </div>   
+                    </div>`
+          for (var j = 0; j < (output.tags).length; j++) {
+            tags_str += `<li id= "${[j]}"><a class="tag" id= "${output.tags[j]+'_'+[j]}">${output.tags[j]}</a></li>`
+          }
+
+        $('.question_section').append(str)
+        $('.tags').append(tags_str)
+        $('.select_Category').hide()
+        // console.log(str)
+      } else {
+        $('.select_Category').show()
+        $('.question_section .row').hide()
+      }
+
+
+    },
+    error: function (err) {
+      console.log(err)
+    }
+  });
+})
